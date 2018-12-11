fb4c8b4920db14cec8b9ddabfdfd478876796880
Sérgio Barbosa <sergiobarbosaweb@gmail.com>
2018-12-11 16:51:45 -0300

    desafio 10 quase resolvido. (testar, revisar e criar demais operadores)

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -71,7 +71,18 @@ parâmetros;
 operador passado para a função "calculator", e passando para esse método
 os dois parâmetros da função de retorno de "calculator".
 */
-// ?
+function calculator ( operator ){
+	if ( !isOperatorValid ( operator ) ){
+		return false;	
+
+	}
+	return function( x, y ){
+			if( typeof x !== 'number' && typeof y !== 'number'){
+				return false;
+			}
+			return operation[ operator ]( x, y )
+	};
+}
 
 /*
 Crie uma função chamada "showOperationMessage" que recebe três parâmetros:
@@ -80,7 +91,11 @@ deve ser a frase:
 'A operação [NUMBER1] [OPERATOR] [NUMBER2] =';
 Essa função mostrará a mensagem da operação que criaremos mais abaixo.
 */
-// ?
+function showOperationMessage(operator, number1, number2){
+
+		return 'A operação ' + number1 + ' ' + operator + ' ' + number2 + ' =';
+
+}
 
 /*
 Crie uma função chamada "showErrorMessage" que recebe um parâmetro: o
@@ -88,7 +103,11 @@ operador da operação cálculo, quando a operação não for válida.
 Essa função deverá retornar a frase:
 'Operação "[OPERATOR]" não permitida!'
 */
-// ?
+function showErrorMessage (operator){
+
+		return 'Operação "' + operator + '" não permitida!';
+
+}
 
 /*
 Nossa calculadora está pronta! Agora vamos testá-la:
@@ -96,7 +115,9 @@ PASSO 1:
 - Declare 3 variáveis: "number1" e "number2", iniciando com valor zero, e
 "operationSignal", sem valor por enquanto.
 */
-// ?
+var number1 = 0;
+var number2 = 0;
+var operationSignal;
 
 /*
 PASSO 2:
@@ -104,7 +125,8 @@ Atribua à variável operationSignal o operador de soma, e declare uma
 variável chamada "sum", que receba a função "calculator", passando por
 parâmetro a variável que recebeu o sinal da operação.
 */
-// ?
+operationSignal = '+';
+var sum = calculator( operationSignal );
 
 /*
 PASSO 3:
@@ -118,7 +140,17 @@ parâmetros para o método "log" de "console":
 - O segundo, a função de soma, passando os dois operandos.
 - Se "sum" for "false", mostrar no console a mensagem de erro.
 */
-// ?
+if ( sum ){
+
+	number1 = 10;
+	number2 = 12;
+	console.log( showOperationMessage(operationSignal, number1, number2), sum(number1, number2) );
+
+}
+
+else{
+	console.log(showErrorMessage( operationSignal ) );
+}
 
 /*
 Repita desde o "PASSO 2" com as operações de subtração, multiplicação,
