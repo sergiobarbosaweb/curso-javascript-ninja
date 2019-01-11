72218050da88f3881d0c13a20c9b0a4b00962832
Sérgio Barbosa <sergiobarbosaweb@gmail.com>
2019-01-11 16:50:54 -0300

    desafio 19 resolvido

--- challenge-19/challenge-19.js
+++ challenge-19/challenge-19.js
@@ -85,6 +85,17 @@ para exemplificar.
 */
 var markup = '<main>\n  <div class="container">\n    <span class="text date"></span>\n    <p class=\'excerpt\'></p>\n  </div>\n</main>';
 console.log( '\nQuais classes CSS existem na marcação abaixo?\n\n', markup, '\n' );
-// ?
+function hasClass( markup, cssClass ){
+	var regex = new RegExp( 'class=["\'].*'+ cssClass +'.*["\']' ); //regex reduzida a partir do mais longo
+	return regex.test( markup ); 
+
+	//regex do exemplo anterior mais longo: class=["\'](?:[\\w\\s]+)?'+ cssClass +'(?:[\\w\\s]+)?["\']
+}
+
+
+var classes = [ 'container', 'text', 'date', 'excerpt', 'main' ];
+classes.forEach(function(cssClass){
+	console.log( hasClass( markup, cssClass ) + ' para a classe ' + cssClass);
+});
 
 })();
